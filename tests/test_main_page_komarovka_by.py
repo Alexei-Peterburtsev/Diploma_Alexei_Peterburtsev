9b5d9cf5c3e0187e02b5a28038c21d5d503a362f
Alexei-Peterburtsev <alexeipeterburtsev@gmail.com>
2024-12-03 14:28:27 +0300

    project v0.45

--- tests/test_main_page_komarovka_by.py
+++ tests/test_main_page_komarovka_by.py
@@ -24,13 +24,65 @@ def test_header_btn(web_browser):
                             (page.header_btn_services, "Услуги", "https://komarovka.by/uslugi/"),
                             (page.header_btn_appeals, "Обращения", "https://komarovka.by/elektronnye-obrashcheniya/"),
                             (page.header_btn_ru, "RU", "https://komarovka.by/"),
-                            (page.header_btn_by, "BY", "https://komarovka.by/by/")
+                            (page.header_btn_by, "BY", "https://komarovka.by/by/"),
+                            # (page.header_btn_facebook, "", "https://www.facebook.com/komarovka.by/"),
+                            # (page.header_btn_instagram, "", "https://www.instagram.com/komarovka.by/"),
+                            # (page.header_btn_tiktok, "", "https://www.tiktok.com/@komarovka.by")
                             ]
 
-    with allure.step("Тест проверки отображения логотипа"):
-        check.is_true(page.header_img.is_visible(), "Логотип отсутствует")
+    header_special_elements = [(page.header_btn_special_ver, "Версия для слабовидящих"),
+                              (page.header_img, "Логотип"),
+                              (page.header_input_find, "Поиск по сайту")
+                              ]
+
+    for special_elements, text_special_elements in header_special_elements:
+        with allure.step("Тест проверки отображения на экране"):
+            check.is_true(special_elements.is_visible(), f"Элемент '{text_special_elements}' отсутствует на экране")
+        with allure.step("Тест проверки кликабельности"):
+            check.is_true(special_elements.is_clickable(), f"Элемент '{text_special_elements}' не кликабелен")
 
     for elements, elements_text, elements_url in header_elements_menu:
+        with allure.step("Тест проверки отображения на экране"):
+            check.is_true(elements.is_visible(), f"Элемент '{elements_text}' отсутствует на экране")
+        with allure.step("Тест проверки кликабельности"):
+            check.is_true(elements.is_clickable(), f"Элемент '{elements_text}' не кликабелен")
+        with allure.step("Тест проверки орфографии"):
+            check.equal(elements.get_text(), elements_text, f"Текст элемента '{elements_text}' содержит ошибку")
+        with allure.step("Тест проверки правильного адреса URL"):
+            check.equal(elements.get_attribute("href"), elements_url, f"Элемент '{elements_text}' содержит неправильную ссылку url")
+        with allure.step("Тест проверки правильного адреса при переходе"):
+            elements.click()
+            check.equal(page.get_current_url(), elements_url, f"Элемент '{elements_text}' переходит на неправильную ссылку url")
+
+def test_futer_btn(web_browser):
+    '''Этот тест проверяет кнопки меню футера на наличие логотипа, кнопки на кликабельность, отображение, орфографию, ссылку url, переход по url'''
+
+    page = MainPage(web_browser)
+
+    futer_btn_elements = [(page.futer_btn_about, "О нас", "https://komarovka.by/about/"),
+                          (page.futer_btn_buyer, "Покупателю", "https://komarovka.by/pokupatelyu/"),
+                          (page.futer_btn_tenant, "Арендатору", "https://komarovka.by/arendatoru/"),
+                          (page.futer_btn_services, "Услуги", "https://komarovka.by/uslugi/"),
+                          (page.futer_btn_appeals, "Обращения", "https://komarovka.by/elektronnye-obrashcheniya/"),
+                          (page.futer_btn_contact, "Контакты", "https://komarovka.by/about/contacts/"),
+                          (page.futer_btn_questions, "Вопросы и ответы", "https://komarovka.by/faq/"),
+                          (page.futer_btn_scheme, "Схема рынка", "https://komarovka.by/pokupatelyu/skhema-rynka/"),
+                          (page.futer_btn_map, "Карта сайта", "https://komarovka.by/sitemap/"),
+                          (page.futer_btn_dining, "Столовая", "https://komarovka.by/pokupatelyu/sobstvennoe-proizvodstvo/stolovaya-komarovka/"),
+                          (page.futer_btn_travel, "Схема проезда", "https://komarovka.by/about/skhema-proezda/"),
+                          (page.futer_btn_vacancies, "Вакансии", "https://komarovka.by/about/vacancii/")
+                          ]
+
+    futer_special_elements = [(page.futer_img, "Логотип"),
+                              (page.futer_telefon, "Номер телефона"),
+                              (page.futer_email, "Электронная почта")
+                              ]
+
+    for special_elements, text_special_elements in futer_special_elements:
+        with allure.step("Тест проверки отображения на экране"):
+            check.is_true(special_elements.is_visible(), f"Элемент '{text_special_elements}' отсутствует на экране")
+
+    for elements, elements_text, elements_url in futer_btn_elements:
         with allure.step("Тест проверки отображения на экране"):
             check.is_true(elements.is_visible(), f"Элемент '{elements_text}' отсутствует на экране")
         with allure.step("Тест проверки кликабельности"):
