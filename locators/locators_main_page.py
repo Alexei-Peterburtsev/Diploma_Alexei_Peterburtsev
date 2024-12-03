5ddc7c33e056e03f095eac428fcd183c010af341
Alexei-Peterburtsev <alexeipeterburtsev@gmail.com>
2024-12-03 17:00:24 +0300

    project v0.48

--- locators/locators_main_page.py
+++ locators/locators_main_page.py
@@ -14,52 +14,31 @@ class MainPage(WebPage):
         super().__init__(web_driver, url)
 
     header_btn_about = WebElement(xpath="//li[@class='with-submenu']//a[contains(text(),'О нас')]")
-    header_btn_buyer = WebElement(xpath="//li[@class='with-submenu']//a[contains(text(),"
-                                        "'Покупателю')]")
-    header_btn_tenant = WebElement(xpath="//li[@class='with-submenu']//a[contains(text(),"
-                                         "'Арендатору')]")
-    header_btn_services = WebElement(xpath="//li[@class='with-submenu']//a[contains(text(),"
-                                           "'Услуги')]")
-    header_btn_appeals = WebElement(xpath="//li[@class='with-submenu']//a[contains(text(),"
-                                          "'Обращения')]")
+    header_btn_buyer = WebElement(xpath="//li[@class='with-submenu']//a[contains(text(),'Покупателю')]")
+    header_btn_tenant = WebElement(xpath="//li[@class='with-submenu']//a[contains(text(),'Арендатору')]")
+    header_btn_services = WebElement(xpath="//li[@class='with-submenu']//a[contains(text(),'Услуги')]")
+    header_btn_appeals = WebElement(xpath="//li[@class='with-submenu']//a[contains(text(),'Обращения')]")
     header_btn_ru = WebElement(xpath="//a[@class='active']")
     header_btn_by = WebElement(xpath="//a[normalize-space()='BY']")
-    header_btn_facebook = WebElement(xpath="//div[@class='header-social hidden-mobile"
-                                           " hidden-tablet']//a[1]")
-    header_btn_instagram = WebElement(xpath="//div[@class='header-social hidden-mobile"
-                                            " hidden-tablet']//a[2]")
-    header_btn_tiktok = WebElement(xpath="//div[@class='header-social hidden-mobile"
-                                         " hidden-tablet']//a[3]")
+    header_btn_facebook = WebElement(xpath="//div[@class='header-social hidden-mobile hidden-tablet']//a[1]")
+    header_btn_instagram = WebElement(xpath="//div[@class='header-social hidden-mobile hidden-tablet']//a[2]")
+    header_btn_tiktok = WebElement(xpath="//div[@class='header-social hidden-mobile hidden-tablet']//a[3]")
     header_btn_special_ver = WebElement(xpath="//a[@class='special-version hidden-mobile']")
     header_input_find = WebElement(xpath="//div[@class='header-others']//form")
     header_img = WebElement(xpath="//img[@class='img-responsive logo']")
 
     futer_img = WebElement(xpath="//img[@src='/include/logo_footer.svg']")
     futer_telefon = WebElement(xpath="//a[normalize-space()='+375(17)352-66-08']")
-    futer_email = WebElement(xpath="//p[@class='hide-block']"
-                                   "//a[@href='mailto:office@komarovka.by']"
-                                   "[normalize-space()='office@komarovka.by']")
-    futer_btn_about = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),"
-                                       "'О нас')]")
-    futer_btn_buyer = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),"
-                                       "'Покупателю')]")
-    futer_btn_tenant = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),"
-                                        "'Арендатору')]")
-    futer_btn_services = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),"
-                                          "'Услуги')]")
-    futer_btn_appeals = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),"
-                                         "'Обращения')]")
-    futer_btn_contact = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),"
-                                         "'Контакты')]")
-    futer_btn_questions = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),'"
-                                           "Вопросы и ответы')]")
-    futer_btn_scheme = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),"
-                                        "'Схема рынка')]")
-    futer_btn_map = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),"
-                                     "'Карта сайта')]")
-    futer_btn_dining = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),"
-                                        "'Столовая')]")
-    futer_btn_travel = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),"
-                                        "'Схема проезда')]")
-    futer_btn_vacancies = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),"
-                                           "'Вакансии')]")
+    futer_email = WebElement(xpath="//p[@class='hide-block']//a[@href='mailto:office@komarovka.by'][normalize-space()='office@komarovka.by']")
+    futer_btn_about = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),'О нас')]")
+    futer_btn_buyer = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),'Покупателю')]")
+    futer_btn_tenant = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),'Арендатору')]")
+    futer_btn_services = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),'Услуги')]")
+    futer_btn_appeals = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),'Обращения')]")
+    futer_btn_contact = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),'Контакты')]")
+    futer_btn_questions = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),'Вопросы и ответы')]")
+    futer_btn_scheme = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),'Схема рынка')]")
+    futer_btn_map = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),'Карта сайта')]")
+    futer_btn_dining = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),'Столовая')]")
+    futer_btn_travel = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),'Схема проезда')]")
+    futer_btn_vacancies = WebElement(xpath="//ul[@class='bottom_main_menu']//a[contains(text(),'Вакансии')]")
