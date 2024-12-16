d32cd804e134061f8a22e5f5b62453820a419655
Alexei-Peterburtsev <alexeipeterburtsev@gmail.com>
2024-12-16 11:35:06 +0300

    rev.0.56 change test (ssl/warning off)

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -1,8 +1,10 @@
 from locust import HttpUser, SequentialTaskSet, task, between
+import urllib3
 
 class User1(HttpUser):
 
     @task
 
     def MainPage(self):
+        urllib3.disable_warnings()
         self.client.get("/", verify=False)
