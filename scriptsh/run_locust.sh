271e71beb2b7b257f15863fd0017172522e45dcb
Alexei-Peterburtsev <alexeipeterburtsev@gmail.com>
2024-12-24 09:27:05 +0300

    v.0.6.3 locust autostart autoquit

--- scriptsh/run_locust.sh
+++ scriptsh/run_locust.sh
@@ -4,4 +4,5 @@ cd ..
 cd tests
 
 start http://localhost:8089
-locust -f locustfile.py --host=https://komarovka.by --run-time=10s --autoquit=1 --autostart
+
+locust -f locustfile.py --host https://komarovka.by --run-time 10 --autostart --autoquit 10
